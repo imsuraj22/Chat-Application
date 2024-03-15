774ea7b5c219a4b61be223a5fd295388e1688e95
Suraj <surajgaikwad9881313856@gmail.com>
2024-03-15 18:46:30 +0530

    added fetch user functionality

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -47,15 +47,6 @@ function handleInputErrors(username,password) {
 		return false;
 	}
 
-	if (password !== confirmPassword) {
-		toast.error("Passwords do not match");
-		return false;
-	}
-
-	if (password.length < 6) {
-		toast.error("Password must be at least 6 characters");
-		return false;
-	}
 
 	return true;
 }
