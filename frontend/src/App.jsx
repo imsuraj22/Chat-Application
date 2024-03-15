34bc84e2379a1bad824dc466ff4e3dde75ab0a2b
Suraj <surajgaikwad9881313856@gmail.com>
2024-03-15 16:07:05 +0530

    login logic added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
-
+  const {authUser}=useAuthContext();
 
   return (
    <div>
     <div className='p-4 flex items-center justify-center h-screen'>
 
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/' element={authUser ? <Home/> : <Navigate to={"/login"} />} />
+        <Route path='/login' element={authUser ? <Navigate to='/' /> : 
+          <Login/>}  />
+        <Route path='/signup' element={authUser ? <Navigate to='/' /> : 
+          <SignUp/>} />
       </Routes>
-      
+      <Toaster/>
     </div>
 
    </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
+import { AuthcontextProvider } from './context/AuthContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthcontextProvider>
     <App />
+    </AuthcontextProvider>
+    
     </BrowserRouter>
   </React.StrictMode>,
 )
